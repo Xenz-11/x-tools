0edc5b43bb43d3705ba143b5e8737f3e35f8d2eb
InuGans <102963507+Xenz-11@users.noreply.github.com>
2022-04-18 11:37:03 +0700

    Add files via upload

---
+++ xtools.sh
@@ -0,0 +1,210 @@
+# Warna
+bi='\e[1;34m'
+hij='\e[1;32m'
+un='\e[1;35m'
+bm='\e[1;36m'
+me='\e[1;31m'
+pu='\e[1;37m'
+ku='\e[1;33m'
+hit='\e[30;1m'
+hi='\e[32;1m'
+
+clear
+
+clear
+echo -e $ku"     _  __             __________  ____  __   _____"
+echo -e $me"    | |/ /            /_  __/ __ \/ __ \/ /  / ___/"                                                                    
+echo -e $hi"    |   /   ______     / / / / / / / / / /   \__\\"
+echo -e $bi"   /   |   /_____/    / / / /_/ / /_/ / /______/ /"
+echo -e $hij"  /_/|_|             /_/  \____/\____/_____/____/"
+echo -e $bm"--------------------------------------------------------------"
+toilet -f wideterm "      Wellcome To X-Tools"|lolcat
+echo -e $bm"--------------------------------------------------------------"
+toilet -f wideterm "Creation By:@Xenz"|lolcat
+toilet -f wideterm "Github     :github.com/Xenz-11"|lolcat
+toilet -f wideterm "Facebook   :Xenz Why"|lolcat
+echo -e $bm"--------------------------------------------------------------"
+echo
+# MEMEK TAYO
+
+
+echo -e $ku""
+echo "╭────────────────────────────[ PILIHAN MENU ]─────────────────────────────╮"
+echo "│ ╭─────────────────────────────────────────────────────────────────────╮ │"
+echo -e "│ │$hi [$pu 01 $hi]$ku X-Spam                                                       │ │"
+echo -e "│ │$hi [$pu 02 $hi]$ku Spam-V2                                                      │ │"
+echo -e "│ │$hi [$pu 03 $hi]$ku Compile Marshal                                              │ │"
+echo -e "│ │$hi [$pu 04 $hi]$ku Trojans                                                      │ │"
+echo -e "│ │$hi [$pu 05 $hi]$ku MBF                                                          │ │"
+echo -e "│ │$hi [$pu 06 $hi]$ku Oh-My-Zsh                                                    │ │"
+echo -e "│ │$hi [$pu 07 $hi]$ku All Spam                                                     │ │"
+echo -e "│ │$hi [$pu 08 $hi]$ku Xmbf                                                         │ │"
+echo -e "│ │$hi [$pu 09 $hi]$ku Matematika                                                   │ │"
+echo -e "│ │$hi [$pu 10 $hi]$ku Github                                                       │ │"
+echo -e "│ │$hi [$me 00 $hi]$ku Keluar                                                       │ │"
+echo -e $ku"│ ╰─────────────────────────────────────────────────────────────────────╯ │"
+echo -e "╰─────────────────────────────────────────────────────────────────────────╯"
+echo -e $hi""
+read -p "Pilih Bang : [+] " xtools
+if [ $xtools == 1 ] || [ $xtools == 01 ]
+then
+clear
+echo -e $ku"[+] Menginstall X-SPAM...."
+git clone https://github.com/Xenz-11/x-spam
+clear
+echo -e $ku"[+] Menginstall X-SPAM...."
+cd x-spam
+clear
+echo -e $ku"[+] Menginstall X-SPAM...."
+git pull
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+bash main.sh
+fi
+
+
+if [ $xtools == 2 ] || [ $xtools == 2 ]
+then
+clear
+echo -e $ku"[+] Menginstall SPAM-V2...."
+git clone https://github.com/Xenz-11/spam-v2
+clear
+echo -e $ku"[+] Menginstall SPAM-V2...."
+cd spam-v2
+clear
+echo -e $ku"[+] Menginstall SPAM-V2...."
+git pull
+clear
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+bash puasa.sh
+fi
+
+if [ $xtools == 3 ] || [ $xtools == 03 ]
+then
+clear
+echo -e $ku"[+] Menginstall COMMARSH...."
+termux-setup-storage git clone
+clear
+echo -e $ku"[+] Menginstall COMMARSH...."
+https://github.com/Xenz-11/commarsh
+clear
+echo -e $ku"[+] Menginstall COMMARSH...."
+cd commarsh
+clear
+echo -e $ku"[+] Menginstall COMMARSH...."
+git pull
+clear
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+python2 marshal.py
+fi
+
+
+if [ $xtools == 4 ] || [ $xtools == 04 ]
+then
+clear
+echo -e $ku"[+] Menginstall TROJANS...."
+git clone https://github.com/Xenz-11/Trojan
+clear
+echo -e $ku"[+] Menginstall TROJANS...."
+cd Trojan
+clear
+echo -e $ku"[+] Menginstall TROJANS...."
+git pull
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+python Trojans.py
+fi
+
+
+if [ $xtools == 5 ] || [ $xtools == 05 ]
+then
+clear
+echo -e $ku"[+] Menginstall MBF...."
+git clone https://github.com/Xenz-11/MBF
+clear
+echo -e $ku"[+] Menginstall MBF...."
+cd MBF
+clear
+echo -e $ku"[+] Menginstall MBF...."
+git pull
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+python mbf.py
+fi
+
+
+if [ $xtools == 6 ] || [ $xtools == 06 ]
+then
+clear
+echo -e $ku"[+] Menginstall OH-MY-ZSH...."
+git clone https://github.com/Xenz-11/Oh-My-Zsh
+clear
+echo -e $ku"[+] Menginstall OH-MY-ZSH...."
+cd Oh-My-Zsh
+clear
+echo -e $ku"[+] Menginstall OH-MY-ZSH...."
+git pull
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+bash oh.sh
+fi
+
+
+if [ $xtools == 7 ] || [ $xtools == 07 ]
+then
+clear
+echo -e $ku"[+] Menginstall ALL SPAM...."
+git clone https://github.com/Xenz-11/all_spam
+clear
+echo -e $ku"[+] Menginstall ALL SPAM...."
+cd all_spam
+echo -e $ku"[+] Menginstall ALL SPAM...."
+clear
+git pull
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+bash run.sh
+fi
+
+if [ $xtools == 8 ] || [ $xtools == 08 ]
+then
+clear
+echo -e $ku"[+] Menginstall XMBF...."
+git clone https://github.com/Xenz-11/Xmbf
+echo -e $ku"[+] Menginstall XMBF...."
+cd Xmbf
+echo -e $ku"[+] Menginstall XMBF...."
+git pull
+echo -e $hi"[+] Install Selesai!!"
+sleep 2
+clear
+python2 mbf.py
+fi
+
+if [ $xtools == 10 ] || [ $xtools == 10 ]
+then
+echo -e $bm"ANDA AKAN DIARAHKAN KE GITHUB"
+sleep 3
+xdg-open https://github.com/Xenz-11
+clear
+fi
+
+if [ $xtools == 00 ] || [ $tools == 0 ]
+clear
+echo "Thanks Dah Pake Toolsnya Bang:)"
+sleep 3
+clear
+exit
+else
+echo -e $hi"Pilih Yang Bener Bang:V"
+bash xtools.sh
+fi
